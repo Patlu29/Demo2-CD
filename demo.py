96f5324def64ac87ceb653e375217410091f2287
PRAKASH RAJAN <153984812+Patlu29@users.noreply.github.com>
2025-03-05 22:33:18 +0530

    simple loop with array (#2)
    merge conflict solved

--- demo.py
+++ demo.py
@@ -3,3 +3,8 @@ age = int(input("Enter your age: "))
 c_name = input("Enter company name: ").upper()
 
 print(f"My name is {name} and my age is {age}. Now im working on {c_name}")
+a = []
+for i in range(1, 10):
+    if i % 2 == 0: 
+        a.append(i)
+print(a)

de19fafcfcf0c880113b0c9cc940d6575370c731
Prakash Rajan <prakashrajan2908@gmail.com>
2025-03-05 21:32:51 +0530

    user input created with print

---
+++ demo.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ").upper()
+age = int(input("Enter your age: "))
+c_name = input("Enter company name: ").upper()
+
+print(f"My name is {name} and my age is {age}. Now im working on {c_name}")
